bd217605b9061fa50f1401fdb67db13464871a28
Edwin Alejandro Cobos Fonseca <alejo740@gmail.com>
2016-08-30 11:03:58 -0500

    ADDED SPLASH ACTIVITY
    Implementation of Splash activity with 3.0 seconds of delay and Logo
    animation.

---
+++ app/src/main/java/com/github/globant/githubsubscribers/commons/utils/Constants.java
@@ -0,0 +1,24 @@
+package com.github.globant.githubsubscribers.commons.utils;
+
+/**
+ * Constants class used to manage all constants of the App code
+ *
+ * @author edwin.cobos
+ * @since 23/08/2016
+ */
+public class Constants {
+
+    public static final String GITHUB_BASE_URL = "https://api.github.com/";
+
+    public static final String DEBUG_PREFIX = "GITHUBAPI";
+    public static final boolean DEBUG_LOGS = true;
+
+    public static final long SPLASH_DELAY = 3000;
+    public final static class SPLASH_LOGO_ANIMATION {
+        public final static long START_DELAY = 200;
+        public final static long DURATION = 1300;
+        public final static float INIT_SCALE = 0.0f;
+        public final static float FINAL_SCALE = 1.0f;
+    }
+
+}
---
+++ app/src/main/java/com/github/globant/githubsubscribers/splash/SplashActivity.java
@@ -0,0 +1,59 @@
+package com.github.globant.githubsubscribers.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.github.globant.githubsubscribers.R;
+import com.github.globant.githubsubscribers.commons.utils.Constants;
+import com.github.globant.githubsubscribers.main.MainActivity;
+
+/**
+ * This is the main activity where the App starts with splash screen
+ *
+ * @author edwin.cobos
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView logoImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        startAnimation();
+        startTimer();
+    }
+
+    /**
+     * Function to set up and play the image's animation
+     */
+    private void startAnimation() {
+        logoImage = (ImageView) findViewById(R.id.splashLogo);
+
+        logoImage.setScaleX(Constants.SPLASH_LOGO_ANIMATION.INIT_SCALE);
+        logoImage.setScaleY(Constants.SPLASH_LOGO_ANIMATION.INIT_SCALE);
+        logoImage.animate().
+                setStartDelay(Constants.SPLASH_LOGO_ANIMATION.START_DELAY).
+                setDuration(Constants.SPLASH_LOGO_ANIMATION.DURATION).
+                scaleX(Constants.SPLASH_LOGO_ANIMATION.FINAL_SCALE).
+                scaleY(Constants.SPLASH_LOGO_ANIMATION.FINAL_SCALE);
+    }
+
+    /**
+     * Function to start the timer delay before changing activity
+     */
+    private void startTimer() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, Constants.SPLASH_DELAY);
+    }
+}
