07db1df35954676ae1fb5489af15365b670d34ac
Edwin Alejandro Cobos Fonseca <alejo740@gmail.com>
2016-09-01 13:06:49 -0500

    MODIFIED ADAPTER TO SET ALL IN VIEWHOLDER
    - Removed SubscriberItem class
    - Modified SubscriberAdapter to set each view in ViewHolder class

--- app/src/main/java/com/github/globant/githubsubscribers/subscriberslist/view/SubscribersAdapter.java
+++ app/src/main/java/com/github/globant/githubsubscribers/subscriberslist/view/SubscribersAdapter.java
@@ -1,10 +1,15 @@
 package com.github.globant.githubsubscribers.subscriberslist.view;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.github.globant.githubsubscribers.R;
 import com.github.globant.githubsubscribers.commons.models.Subscriber;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +18,7 @@ import java.util.List;
  * Adapter class that puts data of subscribers list provided by a service
  *
  * @author juan.herrera
+ * @author edwin.cobos
  * @since 29/08/2016
  */
 public class SubscribersAdapter extends RecyclerView.Adapter<SubscribersAdapter.SubscriberViewHolder> {
@@ -25,8 +31,8 @@ public class SubscribersAdapter extends RecyclerView.Adapter<SubscribersAdapter.
 
     @Override
     public SubscriberViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        SubscriberItem item = new SubscriberItem(viewGroup.getContext());
-        return new SubscriberViewHolder(item);
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.row_subscriber, viewGroup, false);
+        return new SubscriberViewHolder(itemView);
     }
 
     @Override
@@ -47,17 +53,18 @@ public class SubscribersAdapter extends RecyclerView.Adapter<SubscribersAdapter.
 
     public class SubscriberViewHolder extends RecyclerView.ViewHolder {
 
-        private SubscriberItem item;
+        private ImageView image;
+        private TextView text;
 
         public SubscriberViewHolder(View itemView) {
-
             super(itemView);
-            item = (SubscriberItem) itemView;
+            image = (ImageView) itemView.findViewById(R.id.img_avatar_subscriber);
+            text = (TextView) itemView.findViewById(R.id.txt_login_subscriber);
         }
 
-
         public void onBind(Subscriber item) {
-            this.item.onBind(item);
+            text.setText(item.getLogin());
+            Picasso.with(image.getContext()).load(item.getAvataUrl()).into(image);
         }
     }
 }
