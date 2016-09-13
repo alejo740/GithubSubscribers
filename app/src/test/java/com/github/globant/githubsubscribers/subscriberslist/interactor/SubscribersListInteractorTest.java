635299cf5a1cb6418443ba3c3ee70e3e1d5bdc4a
Juan Pablo <juan.herrera@globant.com>
2016-09-13 16:04:11 -0500

    Implemented junit tests for SubriberList according Edwin indications

--- app/src/test/java/com/github/globant/githubsubscribers/subscriberslist/interactor/SubscribersListInteractorTest.java
+++ app/src/test/java/com/github/globant/githubsubscribers/subscriberslist/interactor/SubscribersListInteractorTest.java
@@ -1,24 +1,22 @@
 package com.github.globant.githubsubscribers.subscriberslist.interactor;
 
-import android.os.Environment;
-
 import com.github.globant.githubsubscribers.commons.models.Subscriber;
+import com.github.globant.githubsubscribers.subscriberslist.presenter.SubscribersListPresenter;
 import com.github.globant.githubsubscribers.subscriberslist.presenter.SubscribersListPresenterImpl;
 import com.github.globant.githubsubscribers.subscriberslist.view.SubscribersListView;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
-import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.mockito.MockitoAnnotations;
 
+import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
 
 /**
  * Unit tests for the implementation of {@link SubscribersListInteractorImpl}
@@ -28,23 +26,37 @@ import static org.junit.Assert.assertThat;
  */
 public class SubscribersListInteractorTest {
 
-    @Mock
-    private Call<List<Subscriber>> call;
+    private static final String login = "";
+    private static final String id = "";
+    private static final String avataUrl = "";
 
+    @Mock
     private SubscribersListInteractorImpl interactor;
-    private SubscribersListPresenterImpl presenter;
+
+    @Mock
     private SubscribersListView view;
 
+    @Captor
+    private ArgumentCaptor<SubscribersListInteractor.OnFinishedListener> OnFinishedListenerCallBack;
+
+    private SubscribersListPresenter presenter;
+
     @Before
-    public void createInstanceForInteractor() {
-        interactor = new SubscribersListInteractorImpl();
+    public void setupSubscriberDetailPresenter() {
+        MockitoAnnotations.initMocks(this);
+
+        presenter = new SubscribersListPresenterImpl(view, interactor);
     }
 
     @Test
-    public void getSubscriberList() {
-        presenter = new SubscribersListPresenterImpl(view);
-        interactor.getSubscribersDataList(presenter);
-        assertThat(interactor.call, is(notNullValue()));
+    public void getSubcriberListFromInteractorAndLoadIntoView() {
+
+        List<Subscriber> subscriberList = new ArrayList<>(Arrays.asList(new Subscriber(login, id, avataUrl)));
+
+        presenter.getSubscribersList();
+        verify(interactor).getSubscribersDataList(OnFinishedListenerCallBack.capture());
+        OnFinishedListenerCallBack.getValue().onResponse(subscriberList);
+        view.showSubscribersList(subscriberList);
     }
 
 }
